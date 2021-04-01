f6f94812e88d7c06dd18090c2ce035cfeee945a5
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-01 09:55:34 +0300

    additional changes

--- posts/models.py
+++ posts/models.py
@@ -8,4 +8,9 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __set__(self):
+        return self.tile
 
