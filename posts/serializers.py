f1ccb984d0fbe76e054b9195714e035cb80e8f41
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-08 20:37:27 +0300

    additional changes

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,5 +1,6 @@
 # posts/serializers.py
 
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from .models import Post
 
@@ -9,4 +10,11 @@ class PostSerializer(serializers.ModelSerializer):
     class Meta:
         fields = ('id', 'author', 'title', 'body', 'created_at',)
         model = Post
+
+
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('id', 'username',)
 
