ccb48fc8cdbf9334983270bea4cffaf1268f8a6c
Karma Dorge <37703436+karmadorge@users.noreply.github.com>
2021-04-09 22:17:44 +0300

    additional changes

--- config/urls.py
+++ config/urls.py
@@ -3,6 +3,7 @@
 import debug_toolbar
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +13,9 @@ urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),
     path('api/v1/dj-rest-auth/registration/',
          include('dj_rest_auth.registration.urls')),
+    path('openapi', get_schema_view(
+        title="Blog API",
+        description="A sample API for learning DRF",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ]
